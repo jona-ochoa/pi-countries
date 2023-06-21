2c5fe36981a8b5aadca79b6f079477e53efab38c
jochoa <jonatan.c.ochoa@gmail.com>
2023-06-21 16:06:57 -0300

    added filter activities by Id - backend

--- server/src/controllers/Country/getCountryById.js
+++ server/src/controllers/Country/getCountryById.js
@@ -1,8 +1,27 @@
-const { Country } = require("../../db");
+const { Country, Activity } = require("../../db");
 
 const getCountryById = async (id) => {
   try {
-    const country = await Country.findOne({ where: { id } });
+    let country = await Country.findOne({ where: { id }, include: Activity });
+    country = {
+      id: country.id,
+      name: country.name,
+      flags: country.flags,
+      continents: country.continents,
+      capital: country.capital,
+      subregion: country.subregion,
+      area: country.area,
+      population: country.population,
+      activities: country.activities.map((e) => {
+        return {
+          id: e.id,
+          name: e.name,
+          difficulty: e.difficulty,
+          duration: e.duration,
+          season: e.season
+      }
+      })
+    }
     return country;
   } catch (error) {
     throw error;
