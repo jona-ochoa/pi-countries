13480461b6ed86bb5d66c521c743cfe99862e93b
jochoa <jonatan.c.ochoa@gmail.com>
2023-08-01 21:12:34 -0300

    cors para la nueva actualizacion del deploy

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = require("./routes");
 const morgan = require("morgan");
+const http = require("http");
 const cors = require("cors");
 
 require('./db')
@@ -8,8 +9,10 @@ const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
-server.use(cors());
+server.use(cors({origin: "https://pi-countries-gules.vercel.app"}));
 
 server.use("api/v1", router);
 
-module.exports = server;
+const app = http.createServer(server);
+
+module.exports = app;
